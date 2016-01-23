bf20d2fbff2f23443249cf42c5181b0c06a013df
francisco2611 <paketete2156@hotmail.com>
2016-01-23 10:44:04 -0600

    Update practica1.sql

--- practica1.sql
+++ practica1.sql
@@ -14,3 +14,37 @@ edad:=years*365;
 dbms_output.put_line('tu edad en dias es '||edad);
 end;
 /
+--crear un bloque pl squl que genere 200,000 registros en secuencia ascendente en la tabla dance 
+BEGIN
+  FOR I IN 1..200000 LOOP
+    INSERT INTO DANCE VALUES(I, 'AVILA', 23);
+    
+    END LOOP;
+    END;
+    /
+--HACEMOS UN DELECT PARA VER QUE DIABLOS HIZO
+
+
+SELECT * FROM DANCE;
+
+--sistema de peliculas, sala sera de tarea
+
+create table pelicula (id_pelicula integer,
+                        titulo varchar2(120),
+                        sinopsis varchar2(500),
+                        clasificacion varchar2(5),
+                        constraint pk_id_pelicula primary key(id_pelicula));
+                        
+                        describe pelicula;
+                        
+create table horarios(id_horario integer,
+                      id_pelicula integer,
+                      hprario varchar2(8),
+                      constraint pk_id_horario primary key(id_horario),
+                      constraint fk1_id_pelicula foreign key(id_pelicula) references pelicula(id_pelicula));
+                      
+                      describe horarios;
+                      
+                      
+                        
+
